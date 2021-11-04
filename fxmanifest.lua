0fa212cc2f5cfd2f562e5fddbc534d6f5838a9e9
Collaze <77038583+Collaze@users.noreply.github.com>
2021-11-04 00:52:08 -0500

    Fixed fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Collaze <github.com/collaze>'
+description 'Replace all the food brands across San Andreas with real life food brands!'
+version '1.5.0'
+
+this_is_a_map 'yes'
